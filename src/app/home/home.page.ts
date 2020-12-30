ce79a270ef11830031ca0c2a287f209e803da543
Ishaan Khurana <khuranaishaan5@gmail.com>
2020-12-30 03:56:32 -0500

    12/30/2020 first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { PopoverController } from '@ionic/angular';
-import { MenuComponent } from '../menu/menu.component';
 import { Router } from '@angular/router';
 
 @Component({
